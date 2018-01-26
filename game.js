7ca63bf2ffdc7ff8647e7d192ac6bc792f6d27cc
Maggie Falkenberg <mf1306@yahoo.com>
2018-01-25 22:28:14 -0800

    almost done

--- game.js
+++ game.js
@@ -2,61 +2,102 @@ var allQuestions = [
 {
     question: 'Champagne is a sparkling wine made from grapes grown in the Champagne region of which country?',
     choices: ['France', 'Germany', 'Poland', 'Canada'],
-    correctAnswer: 0
+    correctAnswer: 'France'
 },
 {
     question: 'In which country did cheddar cheese originate?',
     choices: ['England', 'USA', 'Russia', 'Spain'],
-    correctAnswer: 2
+    correctAnswer: 'England'
 },
 {
     question: 'Arby\'s is a fast-food restaurant chain specializing in sandwiches made with what main ingredient?',
     choices: ['Roast beef', 'Lettuce', 'Cheese', 'Tomato'],
-    correctAnswer: 3
+    correctAnswer: 'Roast beef'
 },
 {   
     question: 'What is the main ingredient in thousand island dressing?',
     choices: ['Mayonnaise', 'Mustard', 'Wipping cream', 'Milk'],
-    correctAnswer: 1
+    correctAnswer: 'Mayonnaise'
 },
 {   
     question: 'Sushi is a type of cuisine that originated in what country?',
     choices: ['Japan', 'China', 'India', 'Thailand'],
-    correctAnswer: 1
+    correctAnswer: 'Japan'
 }];
 
-$( "#start" ).click(function() {
+$( "#start" ).click(function() { 
 
+    //( \ ) escape = provides ways to include doublequotes inside a string
     var html = "<form id=\"formQuestion\">";
     for (var i = 0; i < allQuestions.length; i++) {
-        var questionObject = allQuestions[i];
-        var questionString = questionObject.question;
+        var questionString = allQuestions[i].question;
 
-        html = html + "<h2>" + questionString + "</h2>";
-
-        var choicesArray = questionObject.choices;
+        html += "<h2>" + questionString + "</h2>";
+        // var j because it is a for loop in the for loop
+        var choicesArray = allQuestions[i].choices;
         for (var j = 0; j < choicesArray.length; j++) {
             //html = html +
-            // html += "<h3>" + choicesArray[j] + "</h3>";
-            html += "<input type=\"radio\" name=\"question_" + i + "\" value=\"" + j + "\">" + choicesArray[j] + " "; 
+            html += "<input type=\"radio\" name=\"question_" + i + "\" value=\"" + choicesArray[j] + "\">" + choicesArray[j] + " "; 
         }
     } 
     html += "</form>";
-
     $("#content").html(html);
+
     startTimer();
 });
-var i = 10;
+
+var i = 60;
 var timer;
 function startTimer() {
     timer = setInterval(function(){ 
-        $("#timer").html(i--);
+        $("#timer").html("Time remaining: " + i-- + " Seconds");
         if (i<0) {
             clearInterval(timer);
-            alert("End");
+            onFinish();
         }
     }, 1000);
-
 }
+
+function onFinish() {
+    var rightAnswer = 0;
+    var wrongAnswer = 0;
+    var unanswered = 0;
+
+    for (var i = 0; i < allQuestions.length; i++) {
+        var radioValue = $("input[name=question_" + i +  "]:checked").val();
+        var correctAnswerForQuestion = allQuestions[i].correctAnswer;
+        if (typeof radioValue == 'undefined') {
+            unanswered++;
+        } else if (correctAnswerForQuestion == radioValue) {
+            rightAnswer++;
+        } else {
+            wrongAnswer++;
+        }
+    }
+       
+    var allDone = "<h2>All Done!</h2>";
+    allDone += "<ul>";
+    allDone += "<li>Correct Answers: " + rightAnswer + "</li>";
+    allDone += "<li>Incorrect Answers: " + wrongAnswer + "</li>";
+    allDone += "<li>Unanswered: " + unanswered + "</li>";
+    allDone += "</ul>";               
+    $("#content").html(allDone);  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
    
